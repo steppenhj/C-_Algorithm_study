495ba539699e44b5c92e3567ec6bb63823ff0567
박해진 <phj1675@naver.com>
2025-10-31 19:59:54 +0900

    Improve deque handling in main.cpp
    Refactor deque operations for clarity and efficiency.

--- BOJ/스택큐/2346/main.cpp
+++ BOJ/스택큐/2346/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <deque>
+#include <cmath>
 
 //           g++ -o main.exe main.cpp
 //           .\main.exe
@@ -13,28 +14,27 @@ int main(){
 
     int N;
     cin >> N;
-    int x;
     deque<pair<int, int>> dq;
 
     for(int i=0; i<N; i++){
-        cin >> x;
-        dq.push_back(make_pair(x, i+1)); 
+        int paper_value;
+        cin >> paper_value;
+        dq.push_back({paper_value, i+1});
     }
+
     while(!dq.empty()){
-        int current_value = dq.front().first;
+        int move_value = dq.front().first;
         cout << dq.front().second << " ";
         dq.pop_front();
 
-        if(dq.empty()) break;
-
-        if(current_value > 0){
-            for(int i=0; i<current_value-1; i++){
+        if(move_value > 0){
+            for(int i=0; i<move_value-1; i++){
                 dq.push_back(dq.front());
                 dq.pop_front();
             }
-        } 
+        }
         else{
-            for(int i=0; i< (-1)*current_value; i++){
+            for(int i=0; i<abs(move_value); i++){
                 dq.push_front(dq.back());
                 dq.pop_back();
             }

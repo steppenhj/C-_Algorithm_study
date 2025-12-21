423c6661325371772c66fe444191d6cd65b110a6
박해진 <phj1675@naver.com>
2025-12-21 13:37:55 +0900

    Add main.cpp for DP problem 2225 solution
    Implement dynamic programming solution for problem 2225.

---
+++ BOJ/DP/2225/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+//           g++ -o main.exe main.cpp
+//           .\main.exe
+//    g++ -O2 -Wall -std=c++17 -o main main.cpp
+//      ./main
+
+using namespace std;
+
+long long dp[201][201];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N, K;
+    cin >> N >> K;
+
+    for(int i=0; i <= N; i++){
+        dp[1][i] = 1;
+    }
+
+    for(int k=2; k<=K; k++){
+        for(int n=0; n<=N; n++){
+            dp[k][n] = dp[k-1][n] + dp[k][n-1];
+            dp[k][n] %= 100000000;
+        }
+    }
+
+    cout << dp[K][N] << endl;
+
+    return 0;
+}
